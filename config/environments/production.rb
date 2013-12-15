eee1daed3d7c206de0f251ddd4f32bbd6b72e8a4
Sumant Yerramilly <sumant.yerramilly@gmail.com>
2013-12-15 07:32:48 -0800

    Updated User Model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #require for heroku
+  #Note to set this to your actual host name
+config.action_mailer.default_url_options = { :host => 'omr-pincommerce.herokuapp.com/' }
+
 end
